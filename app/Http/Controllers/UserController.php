e332e35870763aad5e76b67bd9e26b98c5c8eb51
Alex Todd <atodd@atodd.io>
2017-10-15 13:38:08 -0500

    View/Update Profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,31 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Auth; //Don't know if this is needed
+use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Image;
+use Storage;
+
 use App\User;
 
 class UserController extends Controller
 {
     public function Profile() {
-        $user = User::where('id', Auth::user()->id)->first();
+        $user = User::with('company')->where('id', Auth::user()->id)->first();
         return view('profile', compact('user'));
     }
 
-    public function UpdateProfile() {
-      $user = User::where('id', Auth::user()->id)->first();
+    public function UpdateProfile(Request $request) {
       $this->validate($request, [
-        'first' => 'required|string|max:255',
-        'last' => 'required|string|max:255',
-        'email' => 'required|email|max:255|unique:users,email,'.$user->id,
-        'avatar' => 'max:255'
+        'first_name' => 'required|string|max:255',
+        'last_name' => 'required|string|max:255',
+        'email' => 'required|email|max:255|unique:users,email,'.Auth::user()->id,
+        'avatar' => 'image|mimes:jpeg,png,jpg|max:2048'
       ]);
 
-      $user->first = $request->input('first');
-      $user->last = $request->input('last');
-      $user->email = $request->input('email');;
-      return Profile();
+      $user = User::where('id', Auth::user()->id)->first();
+      $avatar = $user->avatar;
+      if($request->file('avatar') != ""){
+        $filename = uniqid().'.jpg';
+        Image::make($request->file('avatar'))->encode('jpg')->save(public_path().'/images/profile/'.$filename);
+        $avatar = config('app.url').'/images/profile/'.$filename;
+      }
 
+      $user->update([
+        'first_name' => $request->input('first_name'),
+        'last_name' => $request->input('last_name'),
+        'email' => $request->input('email'),
+        'avatar' => $avatar
+      ]);
+
+      return redirect()->action('UserController@Profile')->with('success', 'Your profile has been updated!');
     }
-
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,35 +2,38 @@
 @section('title', 'Profile')
 
 @section('content')
-
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Heading Row -->
-        <div class="row my-4">
-            <div class="col-lg-8">
-                <img class="img-fluid rounded" src="http://placehold.it/900x400" alt="Subject Photo">
-            </div>
-            <!-- /.col-lg-8 -->
-            <div class="col-lg-4">
-                <h1>Person Name, Event Name, or Company Name</h1>
-                <p>Event or Company Description</p>
-                <a class="btn btn-primary btn-lg" href="#">Website</a>
-            </div>
-            <!-- /.col-md-4 -->
-
-        </div>
-        <!-- /.row -->
-        <a class="btn btn-danger btn-block" href="#">EDIT</a>
-
-
+<div class="container">
+  <div class="row">
+    <div class="col-lg-2">
+      <img class="avatar" src="{{$user->avatar}}" />
     </div>
-    <!-- /.row -->
-
+    <div class="col-lg-4">
+      <h1>{{$user->first_name}} {{$user->last_name}}</h1>
+      @if(!is_null($user->company))
+      <p>{{$user->company->name}}</p>
+      @endif
+      <form method="POST" action="{{ url('/profile') }}" enctype="multipart/form-data">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <div class="form-group">
+            <label for="first_name">First Name</label>
+            <input type="text" class="form-control" name="first_name" value="{{ $user->first_name }}" required>
+          </div>
+          <div class="form-group">
+            <label for="last_name">Last Name</label>
+            <input type="text" class="form-control" name="last_name" value="{{ $user->last_name }}" required>
+          </div>
+          <div class="form-group">
+            <label for="email">Email Address</label>
+            <input type="email" class="form-control" name="email" value="{{ $user->email }}" required>
+          </div>
+          <div class="form-group">
+            <label for="avatar">Profile Picture</label>
+            <input type="file" class="form-control" name="avatar">
+          </div>
+          <button type="submit" class="btn btn-primary btn-xl">Update Profile</button>
+        </form>
     </div>
-    <!-- /.container -->
 
-    <!-- Footer -->
+  </div>
+</div>
 @endsection
-
-
